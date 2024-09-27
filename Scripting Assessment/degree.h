e01a8b88ad61d9510c4b51117466c9aba24f3dc7
Oscar Aramburo <50594310+Applemunchies@users.noreply.github.com>
2024-09-27 11:32:38 -0700

    Delete Scripting Assessment directory

--- Scripting Assessment/degree.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
-static const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
